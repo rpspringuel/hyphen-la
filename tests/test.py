1015f6aafbd824f553b9dbeb5f4cc178e0c8acb1
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-03-30 19:26:15 +0200

    add test file

---
+++ tests/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import pyphen
+import sys
+import re
+
+hyphenator = pyphen.Pyphen(filename='../patterns/hyph_la_ecclesiastic.dic',left=1,right=1)
+
+def comparenoncompletehyphens(original, obtained):
+	i = 0
+	for c in obtained:
+		if c == '-':
+			if original[i] == '-':
+				i = i + 1
+		else:
+			if original[i] == '-':
+				return False
+			else:
+				i = i + 1
+	return True
+
+def dotest(filename, allhyphens=True):
+	global hyphenator
+	print('differences in '+filename+':\nproofread result / result obtained by patterns')
+	with open(filename, 'r') as f:
+		for line in f:
+			line = line.strip()
+			line = re.sub('\s*\%.*', '', line)
+			base = line.replace('-', '')
+			new = hyphenator.inserted(base)
+			if allhyphens:
+				if not line == new:
+					print(line+' / '+new)
+			else:
+				if not comparenoncompletehyphens(line, new):
+					print(line+' / '+new)	
+
+dotest('proofreading-Solesmes-1.txt')
+print('\n')
+dotest('proofreading-Flavigny-text-1.txt')
+print('\n')
+dotest('orberg.txt', False)
