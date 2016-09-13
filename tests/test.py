f98d3ae23d24377cb91912eb101551593ef214e2
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-09-13 08:50:21 +0200

    tests with/without accents

--- tests/test.py
+++ tests/test.py
@@ -21,7 +21,7 @@ def comparenoncompletehyphens(original, obtained):
 
 def dotest(filename, allhyphens=True):
 	global hyphenator
-	print('differences in '+filename+':\nproofread result / result obtained by patterns')
+	print('differences in '+filename+':\nproofread result (correct) / result obtained by patterns (incorrect)')
 	with open(filename, 'r') as f:
 		for line in f:
 			line = line.strip()
@@ -33,7 +33,24 @@ def dotest(filename, allhyphens=True):
 					print(line+' / '+new)
 			else:
 				if not comparenoncompletehyphens(line, new):
-					print(line+' / '+new)	
+					print(line+' / '+new)
+
+def deacc(accstr):
+	return accstr.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u').replace('ý', 'y').replace('́', '').replace('ǽ', 'æ')
+
+def dotest_accents(filename):
+	global hyphenator
+	print('differences in '+filename+':\nresult without accent (correct) / result with accent (incorrect)')
+	with open(filename, 'r') as f:
+		for line in f:
+			line = line.strip()
+			line = re.sub('\s*\%.*', '', line)
+			baseacc = line.replace('-', '').lower()
+			basenoacc = deacc(baseacc)
+			resacc = hyphenator.inserted(baseacc)
+			resnoacc = hyphenator.inserted(basenoacc)
+			if not resnoacc == deacc(resacc):
+				print(resnoacc+' / '+resacc)
 
 dotest('proofreading-Claudio-2.txt')
 print('\n')
@@ -45,4 +62,11 @@ dotest('proofreading-Solesmes-3.txt')
 print('\n')
 dotest('proofreading-Flavigny-text-1.txt')
 print('\n')
+dotest('proofreading-Flavigny-2.txt')
+print('\n')
 dotest('orberg.txt', False)
+print('\n')
+dotest_accents('proofreading-Solesmes-2-accents.txt')
+print('\n')
+dotest_accents('proofreading-Solesmes-3-accents.txt')
+print('\n')
